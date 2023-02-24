fb51f263b4d5b5937af61066ac95d4abbfaef4d7
Nepimwewa <wolfaceking@gmail.com>
2023-02-24 00:28:22 -0800

    Finished the initial website design.

--- header.php
+++ header.php
@@ -12,15 +12,10 @@
   <header id="header-id" class="header-content sticky">
     <nav role="navigation" aria-label="Main menu" id="hamburger-menu">
       <button aria-expanded="false" aria-label="Main menu toggle button"  aria-controls="main-menu" href="#menu" id="menu-toggle" class="menu-toggle" onclick="toggleMenu()">
-          <svg aria-hidden="true" focusable="false" fill="none" stroke="currentColor" 
-              viewBox="0 0 24 24" 
-              xmlns="http://www.w3.org/2000/svg">
-              <path stroke-linecap="round" 
-              stroke-linejoin="round" 
-              stroke-width="2" 
-              d="M4 6h16M4 12h16M4 18h16">
-              </path>
-          </svg>
+        <svg aria-hidden="true" focusable="false" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" stroke="currentColor" d="M4 6h16M4 12h16M4 18h16"></path>
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
       </button>
     </nav>
     <?php
@@ -37,7 +32,10 @@
     ?>
     <div id="shopping-cart-menu">
       <button aria-expanded="false" aria-label="shopping cart toggle button" aria-controls="shopping-cart" id="shopping-toggle" onclick="toggleCart()">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
+        </svg>
       </button>
     </div>
     
